b47150b562fcae95fd3d14dd62fe1673eb408966
dre36dre <dre.son36@gmail.com>
2021-02-03 15:41:36 -0300

    criação do front end usando angular 9

---
+++ aplicacaoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aplicaaoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
